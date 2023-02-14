eb5e8f8bbe7231aea285e650a822eb32197b32bf
Bruno Michel Rossi <bruno.htats@gmail.com>
2023-02-13 23:43:42 -0300

    Virtual Mouse

---
+++ intercept_request.py
@@ -0,0 +1,46 @@
+from flask import request, Flask
+from prometheus_client import start_http_server, Histogram, REGISTRY
+import random
+import time
+
+app = Flask("bosta")
+
+request_time = Histogram('http_request_duration_seconds',
+                         'Description of fkdjfkd',
+                         labelnames=("code", "method", "handler"),
+                         buckets=(0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.45, 0.5, 0.55, 0.6, 0.75, 1, 2.5),
+                         registry=REGISTRY
+                         ).labels(code='', method='', handler='')
+
+
+BUCKETS = [0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.45, 0.5, 0.55, 0.6, 0.75, 1, 2.5]
+
+
+@request_time.time()
+def teste_prom(t):
+    time.sleep(t)
+
+
+def define_param(code, method, handler):
+    request_time.labels(code=code, method=method, handler=handler)
+
+    start_http_server(8000)
+    teste_prom(random.random())
+
+
+@app.before_request
+def hook():
+
+    define_param('200', request.method, request.url)
+    # just do here everything what you need...
+
+
+@app.route("/teste")
+def request_lalala():
+    return "hello"
+
+
+@app.route("/bunda")
+def guilherme():
+    return "alele"
+
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,7 +1,12 @@
+'''
 from pytube import YouTube
 
 DOWNLOAD_FOLDER = '/Users/bmrrossi/Downloads'
 video_url = 'https://www.youtube.com/watch?v=85OW74R0qGc'
 video_obj = YouTube(video_url)
-stream = video_obj.streams.get_highest_resolution()
+stream = video_obj.streams.
 stream.download(DOWNLOAD_FOLDER)
+'''
+
+from pytube import YouTube
+YouTube('https://www.youtube.com/watch?v=r6HiVSIzZeQ').streams.first().download()
